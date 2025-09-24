3e035412fb3c3feddfe338481ed0db62ba04fc34
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-09-24 12:21:21 +0700

    hot fix

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = import.meta.env.VITE_REACT_APP_API_URL
-  ? `${import.meta.env.VITE_REACT_APP_API_URL}/api/users`
+  ? `${import.meta.env.VITE_REACT_APP_API_URL}/users`
   : "http://localhost:3000/api/users";
 
 const api = axios.create({
