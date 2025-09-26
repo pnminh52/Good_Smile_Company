cfa580f8523bc256ace833257432fc8f3d38ddd1
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-09-26 07:18:15 +0700

    `Update CORS origin in server.js to allow all origins`

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", "https://good-smile-company-clone-r6f0k66wc.vercel.app", "https://good-smile-company-clone.vercel.app/"],
+  // origin: ["http://localhost:5173", "http://localhost:3000", "https://good-smile-company-clone-r6f0k66wc.vercel.app", "https://good-smile-company-clone.vercel.app/"],
+  origin:"*",
   credentials: true,
 }));
 app.use(express.json());
