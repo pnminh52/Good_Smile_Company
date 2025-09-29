ee56c24ef44dfd453d2e1925ef37941949e31cc3
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-09-30 06:25:12 +0700

    update

--- backend/hashAdmin.js
+++ backend/hashAdmin.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 
 async function hashPassword() {
-  const plainPassword = "1"; // mật khẩu admin
+  const plainPassword = "1";
   const hash = await bcrypt.hash(plainPassword, 10);
   console.log("Hashed password:", hash);
 }
