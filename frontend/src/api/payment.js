ed8ade34e3c6f7873f07551bb54f126ba311216e
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-10-03 19:37:14 +0700

    `Removed unused vnpayIpn function and updated comments`

--- frontend/src/api/payment.js
+++ frontend/src/api/payment.js
@@ -8,6 +8,3 @@ export const createVnpayPayment = (data) =>
 
 export const verifyVnpayPaymentReturn = (params) =>
   axios.get(`${API}/payment-return`, { params });
-
-export const vnpayIpn = (params) =>
-  axios.get(`${API}/ipn`, { params });
