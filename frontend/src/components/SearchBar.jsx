d848f951be3530dc06ed7c79f0a25869e758b2c9
pnminh52 <phamnhatminhsuzumoe52@gmail.com>
2025-10-15 16:42:39 +0700

    `Added rounded corners and flexbox styling to search bar in Section1 component, and added new CSS classes for cache styling in index.css file.`

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div>
+       <div className="default-ltr-cache-dulgtd">
+                    <div className="curve-container">
+                      <div className="default-ltr-cache-1f97ztc"></div>
+                    </div>
+                    <div className="default-ltr-cache-jtcpfi"></div>
+                  </div>
+    </div>
+  )
+}
+
+export default SearchBar
